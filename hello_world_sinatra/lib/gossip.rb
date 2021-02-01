9577b9ddf5b61056a67d8ae3b12b290555b16df0
Jeanne Colmou <jeannecolmou@macbook-pro-de-jeanne.home>
2021-02-01 16:13:15 +0100

    ayooooo bb

---
+++ hello_world_sinatra/lib/gossip.rb
@@ -0,0 +1,22 @@
+require 'csv'
+#require 'pry'
+
+class Gossip
+	attr_accessor :author, :content
+
+  def initialize(author, content)
+      @content = content
+      @author = author
+    end
+
+	def save
+      CSV.open("./db/gossip.csv", "ab") do |csv|
+          csv << [@author, @content]
+      end
+  end
+
+
+
+end
+
+#binding.pry
